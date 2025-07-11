078036947fa78a4f4e41bf99c1a710c82304ce8a
N00bM4T3R <sabinolbn1810@gmail.com>
2025-07-11 10:44:41 -0300

    class position

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,13 @@
 package aplication;
 
+import BoardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Position pos = new Position(0, 0);
+		
+		System.out.println(pos);
 
 	}
 
